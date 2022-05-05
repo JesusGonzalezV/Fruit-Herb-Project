f5ebf9ce427ec500ddc6bfe624cd3e11923af524
JesusGonzalezV <76001504+JesusGonzalezV@users.noreply.github.com>
2022-05-05 12:16:30 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,35 +1,79 @@
-/*
--------------------------------------------------------------------------------
-
-Purpose: The purpose of the Online Herbs Software Applicationis to help users 
-gain more information on curing different types of diseases by listing different
-fruits and herbs that they should consume.
-Authors: Jesus Gonzalez, Francisco Campuzano, Justin Reed
-Date: 5/5/2022
-Course: Software Engineering, Prof. Yilmaz, CS_3321
-
--------------------------------------------------------------------------------
-*/
-
-
-
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <string.h>
+#include <stdio.h>
 using namespace std;
 
 void herbClass() {
+	string line;
 
+	int option;
+	cout << "Choose a herb to view:\n1.Basil\n2.Cilantro\n3.Garlic\n4.Parsley\n";
+	cin >> option;
+
+	if (option == 1) {
+		ifstream myfile;
+		myfile.open("basil.txt");
+		if (myfile.is_open())
+		{
+			char mychar;
+			while (myfile) {
+				mychar = myfile.get();
+				std::cout << mychar;
+			}
+			myfile.close();
+		}
+	}
+	else if (option == 2)
+	{
+		ifstream myfile1;
+		myfile1.open("cilantro.txt");
+		if (myfile1.is_open())
+		{
+			char mychar;
+			while (myfile1) {
+				mychar = myfile1.get();
+				std::cout << mychar;
+			}
+			myfile1.close();
+		}
+	}
+	else if (option == 3)
+	{
+		ifstream myfile2;
+		myfile2.open("garlic.txt");
+		if (myfile2.is_open())
+		{
+			char mychar;
+			while (myfile2) {
+				mychar = myfile2.get();
+				std::cout << mychar;
+			}
+			myfile2.close();
+		}
+	}
+	else if (option == 4)
+	{
+		ifstream myfile3;
+		myfile3.open("parsley.txt");
+		if (myfile3.is_open())
+		{
+			char mychar;
+			while (myfile3) {
+				mychar = myfile3.get();
+				std::cout << mychar;
+			}
+			myfile3.close();
+		}
+	}
 }
 
-// fruitClass function records the user's input for which fruit to view.
 void fruitClass() {
 	string line;
 	
 	int option;
-	cout << "Choose a fruit to view:\n1.Apple\n2.Orange\n";
+	cout << "Choose a fruit to view:\n1.Apple\n2.Orange\n3.Banana\n4.Mango\n";
 	cin >> option;
 	
 	if(option == 1)	{
@@ -59,17 +103,36 @@ void fruitClass() {
 				myfile1.close();
 			}
 	}
-
+	else if (option == 3)
+	{
+			ifstream myfile2;
+			myfile2.open("banana.txt");
+			if (myfile2.is_open())
+			{
+				char mychar;
+				while (myfile2) {
+					mychar = myfile2.get();
+					std::cout << mychar;
+				}
+				myfile2.close();
+			}
+	}
+	else if (option == 4)
+	{
+		ifstream myfile3;
+		myfile3.open("mango.txt");
+		if (myfile3.is_open())
+		{
+			char mychar;
+			while (myfile3) {
+				mychar = myfile3.get();
+				std::cout << mychar;
+			}
+			myfile3.close();
+		}
+	}
 }
 
-// modifyFruitHerb function allows the user choose which fruit to modify.
-void modifyFruitHerb() {
-	cout << "Please choose an option:" << endl
-		<< "1.Modify fruit\n2.Modify herb\n";
-
-}
-
-// inventory function allows the user choose which inventory to see.
 void inventory() {
 	int option;
 	cout << "Choose an inventory option:\n";
@@ -83,13 +146,8 @@ void inventory() {
 	case 2:
 		herbClass();
 	}
-
-
 }
 
-/* searchInventory function allows the customer to search for a fruit or 
-   herb name.
-*/ 
 void searchInventory() {
 	string searchItem;
 	string line;
@@ -108,21 +166,79 @@ void searchInventory() {
 	}
 }
 
-// adminUserInterface function allows the admin to modify or add a fruit.
+void customerOrder() {
+	string searchOrder;
+	string line;
+	cout << "Enter the customer's order number: ";
+	cin >> searchOrder;
+	ifstream myfile;
+	myfile.open(searchOrder);
+	if (myfile.is_open())
+	{
+		char mychar;
+		while (myfile) {
+			mychar = myfile.get();
+			std::cout << mychar;
+		}
+		myfile.close();
+	}
+}
+
+void modifyFruitHerb() {
+	string stock, ID, date, fruitName, herbName;
+	int option;
+	
+	cout << "Please choose an option :\n1.Modify fruit\n2.Modify herb\n";
+	cin >> option;
+	if (option == 1){
+			cout << "Enter the fruit you want to add: ";
+			cin >> fruitName;
+			ofstream myfile(fruitName);
+			cout << "Enter stock number: ";
+			cin >> stock;
+			cout << "Enter fruit ID: ";
+			cin >> ID;
+			cout << "Enter date: ";
+			cin >> date;
+			myfile << "Total stock: ";
+			myfile << stock;
+			myfile << "\nFruit ID: ";
+			myfile << ID;
+			myfile << "\nDate added: ";
+			myfile << date;
+			myfile.close();
+	}
+	else {
+			cout << "Enter the herb you want to add: ";
+			cin >> herbName;
+			ofstream myfile1(herbName);
+			cout << "Enter stock number: ";
+			cin >> stock;
+			cout << "Enter herb ID: ";
+			cin >> ID;
+			cout << "Enter date: ";
+			cin >> date;
+			myfile1 << "Total stock: ";
+			myfile1 << stock;
+			myfile1 << "\nHerb ID: ";
+			myfile1 << ID;
+			myfile1 << "\nDate added: ";
+			myfile1 << date;
+			myfile1.close();
+	}
+}
+
 void adminUserInterface() {
 	int choiceNum;
 	cout << "Please choose an option:\n" << endl
 		<< "1.Modify or add fruit/herb\n2.Check inventory\n";
 	cin >> choiceNum;
-	switch (choiceNum) {
-	case 1:
+	if (choiceNum == 1) 
 		modifyFruitHerb();
-	case 2:
+	else if (choiceNum == 2)
 		inventory();
-	}
 }
 
-// userNameVerification function verifies that the admin username is correct.
 string userNameVerification(string userName) {
 	while (true) {
 		if (userName == "admin")
@@ -133,7 +249,6 @@ string userNameVerification(string userName) {
 	}
 }
 
-// staffUserInterface function allows staff to choose between options.
 void staffUserInterface() {
 	int option;
 	cout << "Welcome to the staff user interface" << endl;
@@ -143,11 +258,10 @@ void staffUserInterface() {
 		inventory();
 	else if (option == 2)
 		searchInventory();
+	else if (option == 3)
+		customerOrder();
 }
 
-/* main function will present the first set of options for either the
-   customer or the admin.
-*/
 int main() {
 	string userName;
 	int choiceNum;
@@ -164,16 +278,3 @@ int main() {
 	}
 	return 0;
 }
-
-/*
--------------------------------------------------------------------------------
-
-Purpose: The purpose of the Online Herbs Software Applicationis to help users 
-gain more information on curing different types of diseases by listing different
-fruits and herbs that they should consume.
-Authors: Jesus Gonzalez, Francisco Campuzano, Justin Reed
-Date: 5/5/2022
-Course: Software Engineering, Prof. Yilmaz, CS_3321
-
--------------------------------------------------------------------------------
-*/
